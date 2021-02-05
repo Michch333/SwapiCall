ddcb6b61b0633b3cc8503f4dbd0d29a0a42f3f13
Jason Walser <r0flcopt0rz10611@gmail.com>
2021-02-04 21:49:36 -0500

    tried to combine view models to display multiple data points in the same view

---
+++ StarWarsPlanetNames/Models/PeoplePlanetCombinedModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StarWarsPlanetNames.Models
+{
+    public class PeoplePlanetCombinedModel
+    {
+        public List<PeopleModel> PeopleInfo { get; set; }
+        public PlanetModel PlanetModel { get; set; }
+    }
+}
